ae87ed94b0c9592755ad86f9609bb80acf7b5334
unknown <mspagnol@frba.utn.edu.ar>
2022-12-22 11:12:35 -0300

    Entrega 2

--- js/simulador.js
+++ js/simulador.js
@@ -1,14 +1,62 @@
+//Classes
+class Product {
+
+    constructor(name, price, code){
+        this.name = name;
+        this.price = parseFloat(price);
+        this.code = code;
+    }
+
+}
+
+const yeezy = new Product ( "yeezy","260", "Y250");
+const airForce = new Product ("airForce", "120", "AF1");
+const airMax = new Product ("air Max 97", "160", "AM97");
+
+class Persona {
+    productos = []
+
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+
+    addProduct(product){
+        this.productos.push(product);
+    } 
+
+    subtotal(){
+        return this.productos.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+    }
+
+    taxes(){
+        return this.subtotal() * 0.21;
+    }
+
+    total(){
+        return this.subtotal() + this.taxes();
+    }
+
+    saludar(){
+        alert(`Welcome to urban Shoes, ${persona1.name} ,how you doing?` )
+    }
+
+}
+
 let nombre = prompt("Enter your name: ");
-let subTotal = 0;
-let quantity = 0;
+let email = prompt("Enter your email: ");
 
-alert("Welcome to urban Shoes, "+nombre);
+const persona1 = new Persona(nombre, email);
 
+saludar();
 chooseProducts();
 showResults();
 
-
 //Functions
+
+function saludar(){
+    persona1.saludar()
+}
 
 function doesTheProductExists(chosenProduct) {
     
@@ -20,36 +68,31 @@ function doesTheProductExists(chosenProduct) {
 
 }
 
-function addProduct (price){
-    subTotal += price;
-    quantity ++; 
-}
-
 function chooseProducts(){
 
-    alert("Press A for Air Force - Press Y for Yeezy - Press M for Air Max - Press any other for Exit");
+    alert("Press A for Air Force \n- Press Y for Yeezy \n- Press M for Air Max \n- Press any other for Exit");
     let chosenProduct = prompt("Chose the shoe that you want: ");
 
     while(doesTheProductExists(chosenProduct)){
 
         switch(chosenProduct){
             case "A":
-                addProduct(120);
+                persona1.addProduct(airForce);
                 break;
             case "Y":
-                addProduct(260);
+                persona1.addProduct(yeezy);
                 break;
             case "M":
-                addProduct(160);
+                persona1.addProduct(airMax);
                 break;
         }
 
         chosenProduct = prompt("Chose the shoe that you want: "); 
     }
+
+    console.log(persona1);
 }
 
 function showResults(){
-    let taxes = subTotal * 0.21;
-    let total = subTotal + taxes;
-    alert("Subtotal: $"+subTotal +" Taxes: $"+taxes +" Total: $"+total);
+    alert(`Subtotal: $ ${persona1.subtotal()} \n Taxes: $ ${persona1.taxes()} \n  Total: $ ${persona1.total()}`);
 }
